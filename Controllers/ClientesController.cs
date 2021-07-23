455e0008a2f0ab2fa30d863360a80752e0ac4b9b
Bruno Sousa de Almeida Oliveira <bruno.almeida.developer@gmail.com>
2021-07-23 12:15:44 -0300

    Criado web api para estudos

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiClientes.Models;
+
+namespace WebApiClientes.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        [HttpGet]
+        public List<Cliente> ObterClientes()
+        {
+            return clientes;
+        }
+
+        [HttpPost]
+        public void InserirCliente(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome));
+            }            
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
